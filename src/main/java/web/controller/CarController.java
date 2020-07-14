8a88d31e826eaf18dd344566ad7a5de4fd5ec043
Khazhainov <danil.khazhainov@gmail.com>
2020-07-14 15:34:24 +0300

    2nd commit

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -3,25 +3,20 @@ package web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
-
-import javax.servlet.http.HttpServletRequest;
-import java.util.Locale;
 
 @Controller
 public class CarController {
 
     @GetMapping(value = "/cars")
-    public String printCarList(ModelMap model, HttpServletRequest request) {
+    public String printCarList(@RequestParam(value = "locale", required = false) String locale,
+                               ModelMap model) {
+        model.addAttribute("header", (locale != null && locale.equals("en")) ? "CARS" : "МАШИНЫ");
+        model.addAttribute("brand",(locale != null && locale.equals("en")) ? "Brand" : "Бренд");
+        model.addAttribute("model", (locale != null && locale.equals("en")) ? "Model" : "Модель");
+        model.addAttribute("year", (locale != null && locale.equals("en")) ? "Year" : "Год");
         model.addAttribute("cars", CarService.getCars());
-        String header = request.getLocale().equals(Locale.ENGLISH) ? "CARS" : "МАШИНЫ";
-        String brand = request.getLocale().equals(Locale.ENGLISH) ? "Brand" : "Бренд";
-        String carModel = request.getLocale().equals(Locale.ENGLISH) ? "Model" : "Модель";
-        String year = request.getLocale().equals(Locale.ENGLISH) ? "Year" : "Год";
-        model.addAttribute("header", header);
-        model.addAttribute("brand", brand);
-        model.addAttribute("carModel", carModel);
-        model.addAttribute("year", year);
         return "cars";
     }
 }
